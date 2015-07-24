367a5d0304ace87f9873225aa3ed0e8859b2621d
seanwooj <seanwoojinkim@gmail.com>
2015-07-24 17:04:20 +0200

    added activeadmin

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,17 +4,9 @@ class Team < ActiveRecord::Base
   friendly_id :slug_candidates, :use => :slugged
 
   has_many :players
-  has_many :games, :through => :team_games
+  # has_many :games, :through => :team_games
 
   validates_presence_of :name
-
-  def team_id= thing
-
-  end
-
-  def team_logo= thing
-
-  end
 
   def slug_candidates
     [
